675c3c83e11de9f12fa792fb71a4c0bf909f2a2e
Alex Wang <yesterda9@gmail.com>
2025-10-30 11:30:57 -0700

    finish day 3

--- src/tiny_llm/attention.py
+++ src/tiny_llm/attention.py
@@ -55,7 +55,11 @@ class SimpleMultiHeadAttention:
 
 
 def causal_mask(L: int, S: int, dtype: mx.Dtype) -> mx.array:
-    pass
+    xrange = mx.arange(L).reshape(L, 1)
+    yrange = mx.arange(S).reshape(1, S)
+    mask = (yrange > (S - L + xrange))
+    mask = mx.where(mask, mx.array(-mx.inf), mx.array(0)).astype(dtype)
+    return mask
 
 
 def scaled_dot_product_attention_grouped(
@@ -65,8 +69,31 @@ def scaled_dot_product_attention_grouped(
     scale: float | None = None,
     mask: mx.array | str | None = None,
 ) -> mx.array:
-    pass
+    if scale is None:
+        scale = 1 / (key.shape[-1] ** 0.5)
 
+    H_q = query.shape[-3]
+    H = value.shape[-3]
+    n_repeats = H_q // H
+    B = query.shape[:-3]
+    L = query.shape[-2]
+    S = key.shape[-2]
+    D = query.shape[-1]
+
+    query = query.reshape(*B, H, n_repeats,  L, D)
+    key = key.reshape(*B, H, 1, S, D)
+    value = value.reshape(*B, H, 1, S, D)
+    scores = (query @ mx.swapaxes(key, -2, -1)) * scale
+
+    if mask is not None:
+        if mask == "causal":
+            mask = causal_mask(L, S, scores.dtype)
+        mask = mx.broadcast_to(mask, (*B, H_q, L, S))
+        mask = mask.reshape(*B, H, n_repeats, L, S)
+        scores = scores + mask
+
+    result = softmax(scores, -1) @ value
+    return result.reshape(*B, H_q, L, D)
 
 def flash_attention(
     query: mx.array,
--- src/tiny_llm/qwen2_week1.py
+++ src/tiny_llm/qwen2_week1.py
@@ -24,14 +24,49 @@ class Qwen2MultiHeadAttention:
         max_seq_len: int = 32768,
         theta: int = 1000000,
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
+        self.bq = bq
+        self.bk = bk
+        self.bv = bv
+        self.max_seq_len = max_seq_len
+        self.theta = theta
+
+        self.rope = RoPE(self.hidden_size // self.num_heads, max_seq_len, theta, traditional=False)
 
     def __call__(
         self,
         x: mx.array,
         mask: mx.array | str | None = None,
     ) -> mx.array:
-        pass
+        B, L, E = x.shape
+        D = E // self.num_heads
+        projection_q = linear(x, self.wq, bias=self.bq).reshape(
+            B, L, self.num_heads, D
+        )
+        projection_k = linear(x, self.wk, bias=self.bk).reshape(
+            B, L, self.num_kv_heads, D
+        )
+        projection_v = linear(x, self.wv, bias=self.bv).reshape(
+            B, L, self.num_kv_heads, D)
+        projection_q = self.rope(projection_q, offset=slice(0, L))
+        projection_k = self.rope(projection_k, offset=slice(0, L))
+        projection_q = projection_q.transpose(0, 2, 1, 3)
+        projection_k = projection_k.transpose(0, 2, 1, 3)
+        projection_v = projection_v.transpose(0, 2, 1, 3)
+        x = scaled_dot_product_attention_grouped(
+            projection_q.astype(mx.float32),
+            projection_k.astype(mx.float32),
+            projection_v.astype(mx.float32),
+            mask=mask,
+        ).astype(x.dtype)
+        x = x.transpose(0, 2, 1, 3).reshape(B, L, self.hidden_size)
+        return linear(x, self.wo)
 
 
 class Qwen2MLP:
